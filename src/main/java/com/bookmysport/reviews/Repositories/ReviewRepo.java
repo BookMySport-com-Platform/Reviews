e2d272800a4bced7c613917a5a39342e4b19ebb1
Anandateertha.dev <vortex1649@gmail.com>
2024-03-01 12:20:24 +0530

    Feat: Add the feature of fetching the sports using service provider ID

--- src/main/java/com/bookmysport/reviews/Repositories/ReviewRepo.java
+++ src/main/java/com/bookmysport/reviews/Repositories/ReviewRepo.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bookmysport.reviews.Models.ReviewDB;
+import java.util.List;
+
 
 @Repository
 public interface ReviewRepo extends JpaRepository<ReviewDB, UUID> {
+    
+    List<ReviewDB> findBySpId(UUID spId);
     
 }
